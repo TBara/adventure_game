95fc4f991f580117ba8696823570540628006b96
Tomas Barabasz <barabast@os1.engr.oregonstate.edu>
2020-05-02 12:01:34 -0700

    Final

--- adventure.c
+++ adventure.c
@@ -41,8 +41,6 @@ char* ROOM_NAMES[] = {
     "Agate"
 };
 
-// Compares all directories with OSU user name prefix
-// Gets the newest directory.
 void getLatestDir(char* latestDir);
 void setRooms(struct Room* rooms[], char *latestDir);
 void CompleteRoom(struct Room* room, FILE* fp);
@@ -57,6 +55,7 @@ int CountLinesInFile(FILE * fp);
 struct Room* FindRoomByType(struct Room* rooms[], char* r_type);
 struct Room* FindRoomByName(struct Room* rooms[], char* r_name);
 
+// Program main driver function 
 int main(){
     char latestDir[40];
     memset(latestDir, '\0', sizeof(char)*40);
@@ -71,15 +70,18 @@ int main(){
     return 0;
 }
 
-void Game(struct Room *rooms[]){
-    
+// Drives game's interface
+void Game(struct Room *rooms[]){  
     struct Room* curr_rm;
     struct Room* next_rm;
+    
+    // Clear the path
     int path[4000];
     memset(path, '\0', 4000);
+    // Clear user's input
     char userInput[256];
     memset(userInput, '\0', 256);
-
+    // Find starting room and assign it to current room
     char *start_room = "START_ROOM";
     curr_rm = FindRoomByType(rooms, start_room);
     assert(curr_rm != 0);
@@ -88,11 +90,12 @@ void Game(struct Room *rooms[]){
     int gameOn = 1;
     do
     {
+        // Print user's current location
         PrintLocation(curr_rm);
         GetInput(userInput);
         next_rm = FindRoomByName(rooms, userInput);
         if ((next_rm) && (strcmp(userInput, next_rm->r_name) == 0) && (strcmp(next_rm->r_type, "END_ROOM") != 0))
-        {
+        {   // Add last room to path taken, inrement steps, continue game
             path[step_cnt] = GetRoomIdx(next_rm->r_name);
             curr_rm = next_rm;
             next_rm = 0;
@@ -100,6 +103,7 @@ void Game(struct Room *rooms[]){
         }
         else if ((next_rm) && (strcmp(userInput, next_rm->r_name) == 0) && (strcmp(next_rm->r_type, "END_ROOM") == 0))
         {
+            // Game is won. Add last room to path taken
             path[step_cnt] = GetRoomIdx(next_rm->r_name);
             step_cnt++;
             printf("YOU HAVE FOUND THE END ROOM. CONGRATULATIONS!\n"
@@ -107,9 +111,10 @@ void Game(struct Room *rooms[]){
                    step_cnt);
             int i;
             for (i = 0; i < (step_cnt); i++)
-            {
+            {   // Print the path taken
                 printf("%s\n", ROOM_NAMES[path[i]]);
             }
+            // Signal loop exit
             gameOn = 0;
         }
         else if (strcmp(userInput, "time") == 0)
@@ -130,12 +135,14 @@ void Game(struct Room *rooms[]){
             pthread_mutex_destroy(&mutex);
         } else
         {
+            // Traveling to a room that is not in the game OR not connected to the current room prints this message
             printf("HUH? I DON’T UNDERSTAND THAT ROOM. TRY AGAIN.\n\n");
 
         }      
     } while (gameOn == 1);   
 }
 
+// Gets and formats the GMT time and writes it to a file
 void* GetTime()
 {
 	char buffer[256];
@@ -150,14 +157,17 @@ void* GetTime()
 	// Save current time, format, put in buffer
 	time_struct = gmtime(&curr_time);
 	//strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", time_struct);
-    strftime(buffer, sizeof(buffer), "%I:%M%p, %A, %B %d, %Y", time_struct);
+    strftime(buffer, sizeof(buffer), "%l:%M%P, %A, %B %d, %Y", time_struct);
 	
 	// Write to file
 	fputs(buffer, time_file);
+    // Output it to screen
+    printf("%s\n\n", buffer);
 	fclose(time_file);
     return NULL;
 }
 
+// Gets index of a room's name from ROOM_NAMES array
 int GetRoomIdx(char* name){
     int i;
     for (i = 0; i < 10; i++)
@@ -169,17 +179,21 @@ int GetRoomIdx(char* name){
     }
 }
 
+// Saves user input by reference
 void GetInput(char* userInput){
     printf("WHERE TO? >");
     scanf("%s", userInput);
     printf("\n");
 }
 
+// Prints current location and possible connctions
+// with desired formatting
 void PrintLocation(struct Room* room){
     printf("CURRENT LOCATION: %s", room->r_name);
     printf("\nPOSSIBLE CONNECTIONS:");
     int i;
     for(i = 0; i < room->conn_cnt; i++){
+        // Connection name followed by either a period or a comma
         if (i == (room->conn_cnt - 1))
         {
             printf(" %s.\n", room->connects[i]);
@@ -190,11 +204,11 @@ void PrintLocation(struct Room* room){
     }
 }
 
+// Finds the first room with a matching name
 struct Room* FindRoomByName(struct Room* rooms[], char* r_name){
     int i;
     for (i = 0; i < ROOM_COUNT; i++)
     {
-        //printf("%s ", rooms[i]->r_type);
         if( strcmp(rooms[i]->r_name, r_name) == 0){
             return rooms[i];
         }
@@ -202,17 +216,19 @@ struct Room* FindRoomByName(struct Room* rooms[], char* r_name){
     return NULL;
 }
 
+// Finds the first room with a matching type
 struct Room* FindRoomByType(struct Room* rooms[], char* r_type){
     int i;
     for (i = 0; i < ROOM_COUNT; i++)
     {
-        if(strcmp(rooms[i]->r_type, r_type) != 1){
+        if(strcmp(rooms[i]->r_type, r_type) == 0){
             return rooms[i];
         }
     }
     return NULL;
 }
 
+// Opens latest directory and reads the files to create rooms
 void setRooms(struct Room* rooms[], char *latestDir){
     //open the most recent directory
     DIR* currDir;
@@ -245,12 +261,13 @@ void setRooms(struct Room* rooms[], char *latestDir){
                 }
                 else
                 {
-                    // Init and give each room a name
+                    // Allocate memory for each room
                     rooms[f_cnt] = malloc(sizeof (struct Room));
                     memset(rooms[f_cnt], '\0', sizeof (struct Room));
-                    rooms[f_cnt]->conn_cnt = 0;
-                    CompleteRoom(rooms[f_cnt], fp);
+                    rooms[f_cnt]->conn_cnt = 0; // Set connection count to 0
+                    CompleteRoom(rooms[f_cnt], fp); // Assigns room types, names, and sets conncetions
                 }
+                // Go to the beginning of the file and close it
                 rewind(fp);
                 fclose(fp);
                 f_cnt++;
@@ -261,6 +278,8 @@ void setRooms(struct Room* rooms[], char *latestDir){
     
 }
 
+// Allocated memory for room names and room types
+// and assigns their values
 void CompleteRoom(struct Room* room, FILE* fp){
     
     char * line = NULL;
@@ -271,12 +290,12 @@ void CompleteRoom(struct Room* room, FILE* fp){
     int readLines = 0;
     int conn_cnt = 0;
     while ((read = getline(&line, &len, fp)) != -1) {
-
-        
+  
         const char* from = line;
         char *to = (char*) malloc(30);
         if (readLines == 0)
         {
+            // Allocate memory for room name and assign value
             room->r_name = malloc(sizeof(char)*strlen(to));
             memset(room->r_name, '\0', sizeof(char)*strlen(to));
             strncpy(to, from+11, 11);
@@ -284,31 +303,33 @@ void CompleteRoom(struct Room* room, FILE* fp){
             sprintf(room->r_name, "%s", to);
         
         } else if(readLines == (lineCnt - 1) ) {
-
+            // Allocate memory for room type and assign value
             room->r_type = malloc(sizeof(char)*strlen(to));
             memset(room->r_type, '\0', sizeof(char)*strlen(to));
-            strncpy(to, from+11, 11);
+            strncpy(to, from+11, 12);
             to[strlen(to)-1] = 0;
             sprintf(room->r_type, "%s", to);
 
         } else if (!(readLines == lineCnt ) && !(readLines == 0) )
         {
+            // Allocate memory for each connection and assign value
             room->connects[conn_cnt] = malloc(sizeof(char)*strlen(to));
             memset(room->connects[conn_cnt], '\0', sizeof(char)*strlen(to));
             strncpy(to, from+14, 11);
             to[strlen(to)-1] = 0;
             sprintf(room->connects[conn_cnt], "%s", to);
-            room->conn_cnt++;
+            room->conn_cnt++; // Increment connection count
             conn_cnt++;
         }
         free(to);
         readLines++;
     }
     
-    if (line)
+    if (line) // Free memory
         free(line);
 }
 
+// Counts lines in file
 int CountLinesInFile(FILE * fp){
     int character = 0;
     int lines = 0;
@@ -317,13 +338,15 @@ int CountLinesInFile(FILE * fp){
         character = fgetc(fp);
         if (character == '\n')
         {
-            lines++;
+            lines++; // Increment when new line found
         }
     }
     rewind(fp);
     return lines;
 }
 
+// Compares all directories with OSU username prefix
+// Gets the newest directory.
 void getLatestDir(char* latestDir)
 {	
     struct stat *buffer = malloc(sizeof(struct stat));
@@ -360,6 +383,7 @@ void getLatestDir(char* latestDir)
 	free(buffer);
 }
 
+// Deallocates dynamically allocated memory
 void Deallocate(struct Room* rooms[]){
     int i;
     for(i = 0; i < ROOM_COUNT; i++){
